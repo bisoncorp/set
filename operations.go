0bfb81566e174f62c443cddb1167d8c8f68f4aa3
Diego Rosalio <63558169+DiegoRosalio@users.noreply.github.com>
2024-05-07 14:25:19 +0200

    Added closure operations.go

--- operations.go
+++ operations.go
@@ -26,3 +26,17 @@ func Subtraction[T comparable](a, b Set[T]) Set[T] {
 func ExclusiveOr[T comparable](a, b Set[T]) Set[T] {
 	return Subtraction(Union(a, b), Intersection(a, b))
 }
+
+type ClosureFunction[T comparable, S any] func(set.Set[T], S) set.Set[T]
+
+func Closure[T comparable, S any](fn ClosureFunction[T,S]) ClosureFunction[T,S] {
+	return func (a set.Set[T], arg S) set.Set[T] {
+		for {
+			b := fn(a.Clone(), arg)
+			if b.IsSubset(a) && b.IsSuperset(a) {
+				return b
+			}
+			a = b
+		}
+	}
+}
